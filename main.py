26c1b865683d4e45795e8767b09af9c4b1c81e87
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2026-04-22 20:53:39 +0000

    Add search functionality to main.py

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def check_file_is_ok(path):
     return False
 
 
+def sort_contact(contacts):
+    """Sort alphabetically a dictionary of contacts using the 'name' key."""
+    sorted_contacts = dict(sorted(contacts.items()))
+
+    return sorted_contacts
+
+
 def load_contacts(path):
     """Load data from a file."""
     contents = path.read_text()
@@ -24,7 +31,7 @@ def ask_new_contact_info(contacts=None):
     """Ask infos about a new contact"""
     if contacts is None:
         contacts = {}
-    
+
     name = input("Enter a new contact name: ").lower()
 
     infos = {"phone": "", "email": ""}
@@ -56,14 +63,41 @@ def add_new_contact(path):
         store_new_content(new_contact, path)
 
 
-def search_for_contact(path):
+def search_for_contact(path, name):
     """Search for a contact in a file."""
+    found_names = []
+    if check_file_is_ok(path):
+        contacts = load_contacts(path)
+        sorted_contacts = sort_contact(contacts)
+
+        if name in contacts:
+            found_names.append(name)
+
+        show_search_results(found_names, sorted_contacts)
+
+    else:
+        print(
+            "The file is empty or does not exist.Please choose option 2 to add a new contact."
+        )
+
+
+def show_search_results(name_list, contacts):
+    """Show a contact found after a successfull search."""
+    if name_list:
+        print("\n--- Search Results ---")
+        for name in name_list:
+            print(f"\n--- {name.title()} ---")
+            for key, value in contacts[name].items():
+                print(f"- {key.title()}: {value}")
+    else:
+        print("\nNo results!")
 
 
 def show_all_contacts(path):
     """Show all contacts in a file."""
     if check_file_is_ok(path):
         contacts = load_contacts(path)
+        contacts = sort_contact(contacts)
 
         for name in contacts.keys():
             print(f"\n--- {name.title()} ---")
@@ -81,7 +115,7 @@ path = Path(filename)
 
 while True:
     action = input(
-        "Choose one of the following options:"
+        "\nChoose one of the following options:"
         "\n1. Show all contacts"
         "\n2. Add a new contact"
         "\n3. Search a contact by name"
@@ -97,6 +131,7 @@ while True:
     elif action == "2":
         add_new_contact(path)
     elif action == "3":
-        search_for_contact(path)
+        name = input("Please enter the name you're searching for: ").lower()
+        search_for_contact(path, name)
     else:
         print("Invalid entry! Please choose a valid option.")
