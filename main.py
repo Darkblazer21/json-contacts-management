d30fae091fd0f98f1e168dbc8ee837baea72944b
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2026-04-22 18:14:27 +0000

    Add functions to show and add contacts

--- main.py
+++ main.py
@@ -1,19 +1,79 @@
+import json
 from pathlib import Path
 
 
-def ask_for_new_contact():
-    """Ask if user wants to add a new contact"""
-    pass
+def check_file_is_ok(path):
+    """Check if a file exists and is not empty."""
+    if path.exists():
+        if path.stat().st_size != 0:
+            return True
+
+    return False
 
 
-def add_new_contact():
+def load_contacts(path):
+    """Load data from a file."""
+    contents = path.read_text()
+    contacts = json.loads(contents)
+    contacts = dict(contacts)
+
+    return contacts
+
+
+def ask_new_contact_info(contacts=None):
+    """Ask infos about a new contact"""
+    if contacts is None:
+        contacts = {}
+    
+    name = input("Enter a new contact name: ").lower()
+
+    infos = {"phone": "", "email": ""}
+    for key in infos.keys():
+        infos[key] = input(f"Enter this person's {key}: ").lower()
+
+    contacts[name] = infos
+
+    return contacts
+
+
+def store_new_content(contents, path):
+    """Store newly added contents in a file."""
+    new_contents = json.dumps(contents)
+    path.write_text(new_contents)
+
+    print("Contact added successfully!")
+
+
+def add_new_contact(path):
     """Add new contact to a file."""
-    pass
+    if check_file_is_ok(path):
+        contacts = load_contacts(path)
+
+        contacts_updated = ask_new_contact_info(contacts=contacts)
+        store_new_content(contacts_updated, path)
+    else:
+        new_contact = ask_new_contact_info()
+        store_new_content(new_contact, path)
+
+
+def search_for_contact(path):
+    """Search for a contact in a file."""
 
 
 def show_all_contacts(path):
     """Show all contacts in a file."""
-    pass
+    if check_file_is_ok(path):
+        contacts = load_contacts(path)
+
+        for name in contacts.keys():
+            print(f"\n--- {name.title()} ---")
+            for key, value in contacts[name].items():
+                print(f"- {key.title()}: {value}")
+
+    else:
+        print(
+            "The file is empty or does not exist.Please choose option 2 to add a new contact."
+        )
 
 
 filename = "contacts.json"
@@ -33,10 +93,10 @@ while True:
         break
 
     if action == "1":
-        print()
+        show_all_contacts(path)
     elif action == "2":
-        print()
+        add_new_contact(path)
     elif action == "3":
-        print()
+        search_for_contact(path)
     else:
         print("Invalid entry! Please choose a valid option.")
